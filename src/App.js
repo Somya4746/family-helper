65c024d74795e0bcc3f29339c82b78f9cc0b55d7
Somya4746 <Somya4746@gmail.com>
2023-12-27 19:02:36 +0530

    Finally talking to backend

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-// Import necessary Amplify and React components
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { Amplify } from 'aws-amplify'; // Named import for Amplify
-import { graphqlOperation } from '@aws-amplify/api-graphql'; // Import graphqlOperation from '@aws-amplify/api-graphql'
-import { listLists } from './graphql/queries'; // Make sure this is the correct query
+import React, { useState, useEffect, useReducer } from 'react';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { generateClient } from 'aws-amplify/api';
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
 import MainHeader from './components/Headers/MainHeader';
 import Lists from './components/Lists/Lists';
 import { Button, Container, Form, Icon, Modal } from 'semantic-ui-react';
 
-
-
-
-
-// Configure Amplify with your aws-exports
 Amplify.configure(awsExports);
 
+const initialState = {
+  title: '',
+  description: ''
+}
+
+function listReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return { ...state, description: action.value };
+    case 'TITLE_CHANGED':
+      return { ...state, title: action.value };
+    default:
+      console.log('Default action for: ', action);
+      return state;
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(listReducer, initialState)
   const [lists, setLists] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const client = generateClient();
 
-  // Fetch list from the API using Amplify.API
   async function fetchList() {
     try {
-      const listData = await Amplify.API.graphql(graphqlOperation(listLists));
+      const listData = await client.graphql({ query: queries.listLists });
       const lists = listData.data.listLists.items;
       setLists(lists);
     } catch (error) {
@@ -34,13 +47,26 @@ export default function App() {
     }
   }
 
-  // Use useEffect to fetch the list when the component mounts
   useEffect(() => {
     fetchList();
   }, []);
 
   function toggleModal(shouldOpen) {
     setIsModalOpen(shouldOpen);
+  }
+
+  async function saveList(){ 
+    const { title, description } = state;
+    try {
+      await client.graphql({
+        query: mutations.createList,
+        variables: { input: { title, description }}
+      });
+      toggleModal(false);
+      fetchList(); // Refresh the list after saving
+    } catch (error) {
+      console.error('Error saving list', error);
+    }
   }
 
   return (
@@ -51,38 +77,37 @@ export default function App() {
         {({ signOut, user }) => (
           <>
             <Container style={{ height: '100vh' }}>
-              {/* Floating Button */}
               <Button className='floatingButton' onClick={() => toggleModal(true)}>
                 <Icon name='plus' className='floatingButton_icon' />
               </Button>
 
               <main>
-                {/* User Greeting and Sign out Button */}
                 <h1>Hello {user.username}</h1>
                 <button className="fluid ui button" onClick={signOut}>Sign out</button>
-
-                {/* Render fetched lists */}
                 <Lists lists={lists} />
               </main>
 
-              {/* Modal Component */}
               <Modal open={isModalOpen} dimmer='blurring'>
                 <Modal.Header> Create your list </Modal.Header>
-                <Modal.Content><Form>
-                  <Form.Input
-                    error={true ? false : { content: 'Please add a name to your list' }}
-                    label='Title'
-                    placeholder='My pretty list'
-                  />
-                  <Form.TextArea
-                    label='Description'
-                    placeholder='Things that my pretty list is about'
-                  />
-                </Form>
+                <Modal.Content>
+                  <Form>
+                    <Form.Input
+                      label='Title'
+                      placeholder='My pretty list'
+                      value={state.title}
+                      onChange={(e) => dispatch({ type: 'TITLE_CHANGED', value: e.target.value })}
+                    />
+                    <Form.TextArea
+                      label='Description'
+                      placeholder='Things that my pretty list is about'
+                      value={state.description}
+                      onChange={(e) => dispatch({ type: 'DESCRIPTION_CHANGED', value: e.target.value })}
+                    />
+                  </Form>
                 </Modal.Content>
                 <Modal.Actions>
                   <Button negative onClick={() => toggleModal(false)}>Cancel</Button>
-                  <Button positive onClick={() => toggleModal(false)}>Save</Button>
+                  <Button positive onClick={saveList}>Save</Button>
                 </Modal.Actions>
               </Modal>
             </Container>
